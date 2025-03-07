18cd9493f7a26fa7b35b50444946e675199d3663
Santu-Hansariafood <Santu@hansariafood.com>
2025-03-07 15:16:46 +0530

    added the code on to the server

--- src/app/(routes)/home/page.jsx
+++ src/app/(routes)/home/page.jsx
@@ -1,17 +1,14 @@
-
-"use client";
-
 import dynamic from "next/dynamic";
 
-const Home = dynamic(()=>import('@/components/ui/Home/Home'))
-const Layout = dynamic(()=>import('@/layout/Layout'))
+const Home = dynamic(() => import("@/components/ui/Home/Home"));
+const Layout = dynamic(() => import("@/layout/Layout"));
 
 const Page = () => {
   return (
     <Layout>
-      <Home/>
+      <Home />
     </Layout>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
--- src/app/(routes)/page.jsx
+++ src/app/(routes)/page.jsx
@@ -2,13 +2,14 @@ import Home from "@/app/components/ui/Home/Home";
 import About from "@/app/components/ui/About/About";
 import Services from "@/app/components/ui/Services/Services";
 
+
 export default function HomePage() {
   return (
     <main>
       <Home />
       <About />
       <Services />
-      <Contact />
+      {/* <Contact /> */}
     </main>
   );
 }
